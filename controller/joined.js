334dcf4803659beb701be3332410a998bd32de6d
Derrick Farris <mr.dcfarris@gmail.com>
2019-04-12 10:39:48 -0500

    Add node IP info for joining and joined routes

--- controller/joined.js
+++ controller/joined.js
@@ -1,8 +1,9 @@
 const joined = (req, res, next) => {
   const publicKey = req.body.publicKey;
   const nodeId = req.body.nodeId;
+  const nodeIpInfo = req.body.nodeIpInfo;
   let Node = global.node;
-  Node.joined(publicKey, nodeId);
+  Node.joined(publicKey, nodeId, nodeIpInfo);
   let resp = {
     received: true
   }
