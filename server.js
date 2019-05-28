3d1eb6f8da189afa1f25ad4a7f6afd86f59941db
Erik Xavier <erik.xam@gmail.com>
2019-05-28 15:42:29 -0300

    Changes monitor-client path to the relative require.resolve

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(helmet());
 
 app.use(cors());
 
-app.use(express.static('./node_modules/monitor-client/'))
+app.use(express.static(require.resolve('monitor-client')))
 
 app.use('/api', APIRoutes);
 
