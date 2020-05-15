01bf5ff6f7b53475cba0ab46331faa45cab6dc02
Thant Sin Toe <mr.thantsintoe@gmail.com>
2020-05-15 08:57:05 +0630

    validate report in the heartbeat API before processing

--- controller/heartbeat.js
+++ controller/heartbeat.js
@@ -1,12 +1,21 @@
+const validateReport = require('../middleware/validateReport')
 const heartbeat = (req, res, next) => {
-  const nodeId = req.body.nodeId;
-  const data = req.body.data;
-  let Node = global.node;
-  Node.heartbeat(nodeId, data);
-  let resp = {
-    received: true
+  const nodeId = req.body.nodeId
+  const data = req.body.data
+  const isReportValid = validateReport(data)
+  if (nodeId && isReportValid) {
+    let Node = global.node
+    Node.heartbeat(nodeId, data)
+    let resp = {
+      received: true
+    }
+    res.status(200).send(resp)
+  } else {
+    let resp = {
+      received: false
+    }
+    res.status(200).send(resp)
   }
-  res.status(200).send(resp);
 }
 
-module.exports = heartbeat;
+module.exports = heartbeat
