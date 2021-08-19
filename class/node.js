30ce3c2aa0f1f821afa37ab8642bc2398409db72
Thant Sin Toe <mr.thantsintoe@gmail.com>
2021-08-19 18:19:38 +0630

    added Logger module and log historical events of nodes. Expose history logs from a socket server

--- class/node.js
+++ class/node.js
@@ -1,168 +1,183 @@
-// const fs = require('fs')
-// const path = require('path')
-// const writeStream = fs.createWriteStream(path.join('.', 'node-reports.json'))
+let Logger = require('./logger')
 
 class Node {
-  constructor () {
-    this.totalTxInjected = 0
-    this.totalTxRejected = 0
-    this.totalTxExpired = 0
-    this.totalProcessed = 0
-    this.avgTps = 0
-    this.maxTps = 0
-    this.lastTotalProcessed = 0
-    this.reportInterval = 1000
-    this.nodes = this._createEmptyNodelist()
-    this.isTimerStarted = false
-    this.crashTimout = 30000
-    this.lostNodeIds = new Map()
-    this.syncStatements = {}
-    this.lostNodes = []
-    this.removedNodes = []
-    this.history = {}
-  }
-
-  _createEmptyNodelist () {
-    return {
-      joining: {},
-      syncing: {},
-      active: {}
-    }
-  }
-
-  joining (publicKey, nodeIpInfo) {
-    this.nodes.joining[publicKey] = { nodeIpInfo }
-  }
-
-  joined (publicKey, nodeId, nodeIpInfo) {
-    delete this.nodes.joining[publicKey]
-    this.nodes.syncing[nodeId] = { publicKey, nodeIpInfo, timestamp: Date.now() }
-    if (!this.history[nodeId]) this.history[nodeId] = {}
-    this.history[nodeId].joined = Date.now()
-    this.history[nodeId].data = {
-      nodeIpInfo,
-      nodeId
-    }
-    console.log("history", this.history)
-  }
-
-  active (nodeId) {
-    delete this.nodes.syncing[nodeId]
-    this.nodes.active[nodeId] = {}
-    this.history[nodeId].active = Date.now()
-  }
-
-  removed (nodeId) {
-    let removedNode = this.nodes.active[nodeId]
-    if (removedNode) {
-      this.removedNodes.push({
-        ip: removedNode.nodeIpInfo.externalIp,
-        port: removedNode.nodeIpInfo.externalPort,
-        nodeId,
-      })
-      if(this.history[nodeId]) this.history[nodeId].removed = Date.now()
-    }
-    delete this.nodes.active[nodeId]
-  }
-
-  syncReport (nodeId, syncStatement) {
-    this.syncStatements[nodeId] = syncStatement
-  }
-
-  heartbeat (nodeId, data) {
-    console.log("Heart beat data")
-    this.nodes.active[nodeId] = data
-    this.nodes.active[nodeId].timestamp = Date.now()
-    this.nodes.active[nodeId].crashed = false
-    console.log("HISTORY", this.history)
-    if(this.history[nodeId]) {
-      this.history[nodeId].heartbeat = Date.now()
-    }
-    if (data.isLost) {
-      this.lostNodeIds.set(nodeId, true)
-    }
-    if (data.isRefuted) {
-      if (this.lostNodeIds.has(nodeId)) {
-        this.lostNodeIds.delete(nodeId)
-      }
-    }
-    this.nodes.active[nodeId].isLost = this.lostNodeIds.get(nodeId)
-
-    if (this.reportInterval !== data.reportInterval) {
-      this.reportInterval = data.reportInterval
-    }
-    this.totalTxInjected += data.txInjected
-    this.totalTxRejected += data.txRejected
-    this.totalTxExpired += data.txExpired
-    this.totalProcessed += data.txProcessed
-
-    if (!this.isTimerStarted) {
-      setTimeout(() => { this.updateAvgAndMaxTps()}, this.reportInterval)
-      this.isTimerStarted = true
+    constructor() {
+        this.totalTxInjected = 0
+        this.totalTxRejected = 0
+        this.totalTxExpired = 0
+        this.totalProcessed = 0
+        this.avgTps = 0
+        this.maxTps = 0
+        this.lastTotalProcessed = 0
+        this.reportInterval = 1000
+        this.nodes = this._createEmptyNodelist()
+        this.isTimerStarted = false
+        this.crashTimout = 30000
+        this.lostNodeIds = new Map()
+        this.syncStatements = {}
+        this.removedNodes = []
+        this.crashedNodes = {}
+        this.history = {}
     }
 
-    // this.avgApplied += (data.txInjected - data.txRejected - data.txExpired)
-    // writeStream.write(JSON.stringify(this.nodes.active[nodeId], null, 2) + '\n')
-  }
-
-  updateAvgAndMaxTps() {
-    let diffRatio = 0
-    if (Object.keys(this.nodes.active).length === 0) return
-    let newAvgTps = Math.round((this.totalProcessed - this.lastTotalProcessed) / (this.reportInterval / 1000))
-    if (this.avgTps > 0) diffRatio = (newAvgTps - this.avgTps) / this.avgTps
-    if ((diffRatio < 1.5) || (diffRatio > 0.5)) {
-      if (newAvgTps > this.maxTps) { this.maxTps = newAvgTps }
+    _createEmptyNodelist() {
+        return {
+            joining: {},
+            syncing: {},
+            active: {}
+        }
     }
-    this.avgTps = newAvgTps
-    this.lastTotalProcessed = this.totalProcessed
-    this.checkDeadOrAlive()
-    setTimeout(() => { this.updateAvgAndMaxTps() }, this.reportInterval)
-  }
 
-  checkDeadOrAlive() {
-    for (let nodeId in this.nodes.active) {
-      if (this.nodes.active[nodeId].timestamp < Date.now() - this.crashTimout) {
-        console.log('Node is dead')
-        this.nodes.active[nodeId].crashed = true
-        this.history[nodeId].crashed = this.nodes.active[nodeId].timestamp
-      } else {
+    joining(publicKey, nodeIpInfo) {
+        this.nodes.joining[publicKey] = {nodeIpInfo}
+    }
+
+    joined(publicKey, nodeId, nodeIpInfo) {
+        delete this.nodes.joining[publicKey]
+        this.nodes.syncing[nodeId] = {publicKey, nodeIpInfo, timestamp: Date.now()}
+        if (!this.history[nodeId]) this.history[nodeId] = {}
+        this.history[nodeId].joined = Date.now()
+        this.history[nodeId].data = {
+            nodeIpInfo,
+            nodeId
+        }
+        Logger.historyLogger.info(`NODE JOINED, NodeId: ${nodeId}, Ip: ${nodeIpInfo.externalIp}, Port: ${nodeIpInfo.externalPort}`)
+    }
+
+    active(nodeId) {
+        delete this.nodes.syncing[nodeId]
+        this.nodes.active[nodeId] = {}
+        this.history[nodeId].active = Date.now()
+        const nodeData = this.history[nodeId].data
+        Logger.historyLogger.info(`NODE ACTIVE, NodeId: ${nodeId}, Ip: ${nodeData.nodeIpInfo.externalIp}, Port: ${nodeData.nodeIpInfo.externalPort}`)
+    }
+
+    removed(nodeId) {
+        let removedNode = this.nodes.active[nodeId]
+        if (removedNode) {
+            this.removedNodes.push({
+                ip: removedNode.nodeIpInfo.externalIp,
+                port: removedNode.nodeIpInfo.externalPort,
+                nodeId
+            })
+            if (this.history[nodeId]) this.history[nodeId].removed = Date.now()
+            Logger.historyLogger.info(`NODE REMOVED, NodeId: ${nodeId}, Ip: ${removedNode.nodeIpInfo.externalIp}, Port: ${removedNode.nodeIpInfo.externalPort}`)
+        }
+        delete this.nodes.active[nodeId]
+    }
+
+    syncReport(nodeId, syncStatement) {
+        this.syncStatements[nodeId] = syncStatement
+    }
+
+    heartbeat(nodeId, data) {
+        // Logger.historyLogger.info(`NODE HEARTBEAT, NodeId: ${nodeId}, Ip: ${data.nodeIpInfo.externalIp}, Port: ${data.nodeIpInfo.externalPort}`)
+        // Logger.mainLogger.info(`NODE HEARTBEAT, NodeId: ${nodeId}, ${JSON.stringify(data)}`)
+        this.nodes.active[nodeId] = data
+        this.nodes.active[nodeId].timestamp = Date.now()
         this.nodes.active[nodeId].crashed = false
-      }
+        if (this.history[nodeId]) {
+            this.history[nodeId].heartbeat = Date.now()
+        }
+        if (data.isLost) {
+            if(!this.lostNodeIds.get(nodeId)) {
+                Logger.historyLogger.info(`NODE LOST, NodeId: ${nodeId}, Ip: ${data.nodeIpInfo.externalIp}, Port: ${data.nodeIpInfo.externalPort}`)
+            }
+            this.lostNodeIds.set(nodeId, true)
+        }
+        if (data.isRefuted) {
+            if (this.lostNodeIds.has(nodeId)) {
+                this.lostNodeIds.delete(nodeId)
+                Logger.historyLogger.info(`NODE REFUTED, NodeId: ${nodeId}, Ip: ${data.nodeIpInfo.externalIp}, Port: ${data.nodeIpInfo.externalPort}`)
+            }
+        }
+        this.nodes.active[nodeId].isLost = this.lostNodeIds.get(nodeId)
+
+        if (this.reportInterval !== data.reportInterval) {
+            this.reportInterval = data.reportInterval
+        }
+        this.totalTxInjected += data.txInjected
+        this.totalTxRejected += data.txRejected
+        this.totalTxExpired += data.txExpired
+        this.totalProcessed += data.txProcessed
+
+        if (!this.isTimerStarted) {
+            setTimeout(() => {
+                this.updateAvgAndMaxTps()
+            }, this.reportInterval)
+            this.isTimerStarted = true
+        }
+
+        // this.avgApplied += (data.txInjected - data.txRejected - data.txExpired)
+        // writeStream.write(JSON.stringify(this.nodes.active[nodeId], null, 2) + '\n')
     }
-  }
 
-  getHistory() {
-    return this.history
-  }
-
-  report () {
-    return {
-      nodes: this.nodes,
-      totalInjected: this.totalTxInjected,
-      totalRejected: this.totalTxRejected,
-      totalExpired: this.totalTxExpired,
-      totalProcessed: this.totalProcessed,
-      avgTps: this.avgTps,
-      maxTps: this.maxTps,
-      timestamp: Date.now()
+    updateAvgAndMaxTps() {
+        let diffRatio = 0
+        if (Object.keys(this.nodes.active).length === 0) return
+        let newAvgTps = Math.round((this.totalProcessed - this.lastTotalProcessed) / (this.reportInterval / 1000))
+        if (this.avgTps > 0) diffRatio = (newAvgTps - this.avgTps) / this.avgTps
+        if ((diffRatio < 1.5) || (diffRatio > 0.5)) {
+            if (newAvgTps > this.maxTps) {
+                this.maxTps = newAvgTps
+            }
+        }
+        this.avgTps = newAvgTps
+        this.lastTotalProcessed = this.totalProcessed
+        this.checkDeadOrAlive()
+        setTimeout(() => {
+            this.updateAvgAndMaxTps()
+        }, this.reportInterval)
     }
-  }
 
-  getSyncReports () {
-    return this.syncStatements
-  }
+    checkDeadOrAlive() {
+        for (let nodeId in this.nodes.active) {
+            if (this.nodes.active[nodeId].timestamp < Date.now() - this.crashTimout) {
+                this.nodes.active[nodeId].crashed = true
+                this.history[nodeId].crashed = this.nodes.active[nodeId].timestamp
+                if (!this.crashedNodes[nodeId]) {
+                    let data = this.nodes.active[nodeId]
+                    this.crashedNodes[nodeId] = data
+                    Logger.historyLogger.info(`NODE DEAD, NodeId: ${nodeId}, Ip: ${data.nodeIpInfo.externalIp}, Port: ${data.nodeIpInfo.externalPort}`)
+                }
+            } else {
+                this.nodes.active[nodeId].crashed = false
+            }
+        }
+    }
 
-  flush () {
-    console.log('Flushing report')
-    this.nodes = this._createEmptyNodelist()
-    this.totalTxInjected = 0
-    this.totalTxRejected = 0
-    this.totalTxExpired = 0
-    this.totalProcessed = 0
-    this.avgTps = 0
-    this.maxTps = 0
-    this.lastTotalProcessed = 0
-  }
+    getHistory() {
+        return this.history
+    }
+
+    report() {
+        return {
+            nodes: this.nodes,
+            totalInjected: this.totalTxInjected,
+            totalRejected: this.totalTxRejected,
+            totalExpired: this.totalTxExpired,
+            totalProcessed: this.totalProcessed,
+            avgTps: this.avgTps,
+            maxTps: this.maxTps,
+            timestamp: Date.now()
+        }
+    }
+
+    getSyncReports() {
+        return this.syncStatements
+    }
+
+    flush() {
+        console.log('Flushing report')
+        this.nodes = this._createEmptyNodelist()
+        this.totalTxInjected = 0
+        this.totalTxRejected = 0
+        this.totalTxExpired = 0
+        this.totalProcessed = 0
+        this.avgTps = 0
+        this.maxTps = 0
+        this.lastTotalProcessed = 0
+    }
 }
 
 module.exports = Node
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { promisify } = require('util')
+const {promisify} = require('util')
 const http = require('http')
 const WebSocket = require('ws')
 const Tail = require('tail').Tail
@@ -15,25 +15,45 @@ const helmet = require('helmet')
 const path = require('path')
 const fs = require('fs')
 const rfs = require('rotating-file-stream')
+const Logger = require('./class/logger')
+const app = express()
+
+const logDirectory = path.join(__dirname, 'req-log')
+const clientModule = require.resolve('monitor-client')
+const clientDirectory = path.dirname(clientModule)
+const viewDirectory = path.join(clientDirectory + '/')
+const staticDirectory = path.resolve(clientDirectory + '/')
+const logsConfig = require('./monitor-log.json')
+const logDir = `monitor-logs`
+const baseDir = '.'
+logsConfig.dir = logDir
+
+let fileWatcher
+const server = http.createServer(app)
+const {Server} = require('socket.io')
+const io = new Server(server)
+let fileSubscribers = {}
+let file = `${baseDir}/${logDir}/history.log`
+const filePath = path.resolve(file)
 
 http.get[promisify.custom] = function getAsync(options) {
-  return new Promise((resolve, reject) => {
-    http
-      .get(options, (response) => {
-        response.end = new Promise((resolve) => response.on('end', resolve))
-        resolve(response)
-      })
-      .on('error', reject)
-  })
+    return new Promise((resolve, reject) => {
+        http
+            .get(options, (response) => {
+                response.end = new Promise((resolve) => response.on('end', resolve))
+                resolve(response)
+            })
+            .on('error', reject)
+    })
 }
 const get = promisify(http.get)
 
 async function getJSON(url) {
-  const res = await get(url)
-  let body = ''
-  res.on('data', (chunk) => (body += chunk))
-  await res.end
-  return JSON.parse(body)
+    const res = await get(url)
+    let body = ''
+    res.on('data', (chunk) => (body += chunk))
+    await res.end
+    return JSON.parse(body)
 }
 
 const Node = require('./class/node')
@@ -43,25 +63,15 @@ const APIRoutes = require('./api')
 // config variables
 const CONFIG = require('./config')
 if (process.env.PORT) {
-  CONFIG.port = process.env.PORT
+    CONFIG.port = process.env.PORT
 }
-
-//Express
-const app = express()
 
 // Initialize node
 const node = new Node()
 global.node = node
 
 // Setup Log Directory
-const logDirectory = path.join(__dirname, 'req-log')
-
-const clientModule = require.resolve('monitor-client')
-const clientDirectory = path.dirname(clientModule)
-const viewDirectory = path.join(clientDirectory + '/')
-const staticDirectory = path.resolve(clientDirectory + '/')
-// const viewDirectory = path.join(clientDirectory + '/views');
-// const staticDirectory = path.resolve(clientDirectory + '/public');
+Logger.initLogger(baseDir, logsConfig)
 
 console.log('absoluteClientPath', clientDirectory)
 console.log('view Directory', viewDirectory)
@@ -76,8 +86,8 @@ fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
 
 // create a rotating write stream
 let accessLogStream = rfs('access.log', {
-  interval: '1d', // rotate daily
-  path: logDirectory,
+    interval: '1d', // rotate daily
+    path: logDirectory
 })
 
 //Morgan
@@ -85,76 +95,71 @@ app.use(morgan('common', { stream: accessLogStream }))
 app.use(morgan('dev'))
 
 // Parse body params and attach them to req.body
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
 app.use(cookieParser())
 app.use(compress())
 app.use(methodOverride())
-
 app.use(helmet())
-
 app.use(cors())
 
-// app.use('/', express.static(path.dirname(require.resolve('monitor-client'))))
 app.get('/', (req, res) => {
-  res.render('index.html', { title: 'test' })
+    res.render('index.html', {title: 'test'})
 })
 app.get('/log', (req, res) => {
-  console.log('log server page')
-  res.render('log.html', { title: 'test' })
+    console.log('log server page')
+    res.render('log.html', {title: 'test'})
 })
 
-// log-streamer remote log viewing client
-app.get('/log2', (req, res) => {
-  console.log('log server page')
-  res.render('log2.html', { title: 'test' })
+app.get('/history-log', (req, res) => {
+    console.log('log server page')
+    res.render('history-log.html', {title: 'test'})
 })
 
-// Node totals summary view for monitoring large networks
 app.get('/summary', async (req, res) => {
-  // Ping a node for the current cycle
-  let cycle = {}
-  let cycleUrl
-  let configUrl
+    // Ping a node for the current cycle
+    let cycle = {}
+    let cycleUrl
+    let configUrl
 
-  const joining = global.node.nodes['joining'] // { [id: string]: { nodeIpInfo: {...} } }
-  const syncing = global.node.nodes['syncing'] // { [id: string]: { nodeIpInfo: {...} } }
-  const active = global.node.nodes['active'] // { [id: string]: { nodeIpInfo: {...} } }
-  const removed = global.node.removedNodes
-  const node = Object.values(active)[0] || Object.values(syncing)[0] || Object.values(joining)[0]
-  if (node) {
-    cycleUrl = `http://${node.nodeIpInfo.externalIp}:${node.nodeIpInfo.externalPort}/sync-newest-cycle`
-    configUrl = `http://${node.nodeIpInfo.externalIp}:${node.nodeIpInfo.externalPort}/config`
-    try {
-      cycle = await getJSON(cycleUrl)
-      cycle = cycle.newestCycle
-    } catch (e) {
-      console.log('Cannot get cycle from node')
+    const joining = global.node.nodes['joining'] // { [id: string]: { nodeIpInfo: {...} } }
+    const syncing = global.node.nodes['syncing'] // { [id: string]: { nodeIpInfo: {...} } }
+    const active = global.node.nodes['active'] // { [id: string]: { nodeIpInfo: {...} } }
+    const removed = global.node.removedNodes
+    const node = Object.values(active)[0] || Object.values(syncing)[0] || Object.values(joining)[0]
+    if (node) {
+        cycleUrl = `http://${node.nodeIpInfo.externalIp}:${node.nodeIpInfo.externalPort}/sync-newest-cycle`
+        configUrl = `http://${node.nodeIpInfo.externalIp}:${node.nodeIpInfo.externalPort}/config`
+        try {
+            cycle = await getJSON(cycleUrl)
+            cycle = cycle.newestCycle
+        } catch (e) {
+            console.log('Cannot get cycle from node')
+        }
     }
-  }
 
-  const summary = {
-    joining: [],
-    syncing: [],
-    active: [],
-  }
-
-  for (const state in summary) {
-    for (const id in global.node.nodes[state]) {
-      const ip = global.node.nodes[state][id].nodeIpInfo.externalIp
-      const port = global.node.nodes[state][id].nodeIpInfo.externalPort
-      const logStreamerServer = encodeURIComponent(`http://${ip}:3334`)
-      summary[state].push(`<a href="log?ip=${ip}&port=${port}" target="_blank">[${ip}]</a>`)
+    const summary = {
+        joining: [],
+        syncing: [],
+        active: []
     }
-  }
 
-  let removedHtmlStr = ``
-  for (let node of removed) {
-    removedHtmlStr += `${node.ip} `
-  }
+    for (const state in summary) {
+        for (const id in global.node.nodes[state]) {
+            const ip = global.node.nodes[state][id].nodeIpInfo.externalIp
+            const port = global.node.nodes[state][id].nodeIpInfo.externalPort
+            const logStreamerServer = encodeURIComponent(`http://${ip}:3334`)
+            summary[state].push(`<a href="log?ip=${ip}&port=${port}" target="_blank">[${ip}]</a>`)
+        }
+    }
 
-  const page = `<!DOCTYPE html>
+    let removedHtmlStr = ``
+    for (let node of removed) {
+        removedHtmlStr += `${node.ip} `
+    }
+
+    const page = `<!DOCTYPE html>
 <html>
   <body>
     cycle: ${cycle && cycle.counter > -1 ? cycle.counter : -1}
@@ -206,65 +211,56 @@ app.get('/summary', async (req, res) => {
 </html>
 `
 
-  res.setHeader('Content-Type', 'text/html')
-  res.send(page)
+    res.setHeader('Content-Type', 'text/html')
+    res.send(page)
 })
 
 app.use('/api', APIRoutes)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const error = new Error('API not found!')
-  error.status = 404
-  return next(error)
+    const error = new Error('API not found!')
+    error.status = 404
+    return next(error)
 })
 
 app.use((err, req, res, next) =>
-  res.status(err.status || 500).json({
-    error: {
-      message: err.message,
-      status: err.status,
-      stack: CONFIG.env === 'development' ? err.stack : {},
-    },
-    status: err.status,
-  })
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message,
+            status: err.status,
+            stack: CONFIG.env === 'development' ? err.stack : {}
+        },
+        status: err.status
+    })
 )
 
-// Web Socket Server to listen new logs
-const server = http.createServer()
-const wss = new WebSocket.Server({ server })
-let socketConnections = {}
 
-wss.on('connection', function connection(ws) {
-  // [AS] Disabled to not crash Monitor when running non-local nodes
+Logger.mainLogger.info(`file: ${file}`)
+Logger.mainLogger.info(`filePath: ${path.resolve(file)}`)
 
-  // ws.on('message', function incoming(data) {
-  //   let { node, slot } = JSON.parse(data)
-  //   console.log('Requested log: ', node)
-  //   console.log('resolved file path', path.resolve(`../../instances/shardus-instance-${node.port}/logs/${node.filename}.log`))
-  //   const filePath = path.resolve(`../instances/shardus-instance-${node.port}/logs/${node.filename}.log`)
-  //   if (socketConnections[slot]) {
-  //     console.log('Unwatching previous log file...')
-  //     socketConnections[slot].unwatch()
-  //     socketConnections[slot] = null
-  //   }
-  //   socketConnections[slot] = new Tail(filePath, { fromBeginning: false })
-  //   socketConnections[slot].watch()
-  //   socketConnections[slot].on('line', (data) => {
-  //     // console.log(`New data detected. Slot`, slot, node.port, node.filename)
-  //     ws.send(
-  //       JSON.stringify({
-  //         slot: slot,
-  //         data: '\n' + data,
-  //       })
-  //     )
-  //   })
-  // })
-})
-
-server.listen(8080, (err) => {
-  if (err) console.log(err)
-  console.log(`Web Socket server is listening at port 8080`)
+io.on('connection', (socket) => {
+    console.log('A client connected', socket.id)
+    if (!fileSubscribers[socket.id]) {
+        fileSubscribers[socket.id] = true
+        fs.readFile(filePath, 'utf-8', (error, data) => {
+            if (!data) {
+                data = 'Found no previous log.'
+            }
+            io.emit('old-data', data)
+        })
+    }
+    socket.on('message', (msg) => {
+        if (!fileWatcher) {
+            fileWatcher = new Tail(filePath, {fromBeginning: false})
+            fileWatcher.watch()
+            fileWatcher.on('line', (data) => {
+                io.emit('new-history-log', data)
+            })
+        } else {
+            console.log('File watcher already existed.')
+        }
+    })
 })
 
 // HTTP server for searching string in a log file
@@ -295,13 +291,14 @@ server.listen(8080, (err) => {
 // })
 
 const start = () => {
-  app.listen(CONFIG.port, (err) => {
-    if (err) {
-      console.error(err)
-      throw err
-    }
-    console.log(`server started on port ${CONFIG.port} (${CONFIG.env})`)
-  })
+    server.listen(CONFIG.port, (err) => {
+        if (err) {
+            console.error(err)
+            throw err
+        }
+        console.log(`server started on port ${CONFIG.port} (${CONFIG.env})`)
+        Logger.historyLogger.info(`Monitor server started.`)
+    })
 }
 
 start()
