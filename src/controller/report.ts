dee8f67e5ecfeb2c830c98c2678f335d4d7b54bd
banky <banky@shardeum.org>
2022-10-21 13:03:45 -0400

    Refactor the report endpoint and remove dead code

--- src/controller/report.ts
+++ src/controller/report.ts
@@ -1,14 +1,13 @@
-import { NextFunction, Request, Response } from "express";
-import { Node } from "../class/node";
+import {NextFunction, Request, Response} from 'express';
+import {Node} from '../class/node';
 
 const report = (req: Request, res: Response, next: NextFunction) => {
   const lastTimestamp = req.query.timestamp as string;
-  console.log(req.query.timestamp);
-  let Node: Node = global.node;
-  let data;
-  if (lastTimestamp) data = Node.report(Number(lastTimestamp));
-  else data = Node.report(Number(lastTimestamp));
-  res.status(200).send(data);
+  const node: Node = global.node;
+
+  const report = node.report(Number(lastTimestamp));
+
+  res.status(200).send(report);
 };
 
 module.exports = report;
