1fc21b6ee392cc67306e1a0770d756cbfe2b49d2
banky <banky@shardeum.org>
2022-12-12 15:39:19 -0500

    Re add accidentally deleted code

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,3 +1,19 @@
+const joining = require('./joining');
+const joined = require('./joined');
+const active = require('./active');
+const removed = require('./removed');
+const rareCounter = require('./rareCounter');
+const resetRareCounter = require('./resetRareCounter');
+const getRemoved = require('./getRemoved');
+const heartbeat = require('./heartbeat');
+const report = require('./report');
+const flush = require('./flush');
+const syncReport = require('./syncReport');
+const getSyncReports = require('./getSyncReports');
+const getTxCoverage = require('./txCoverage');
+const history = require('./history');
+const getScaleReports = require('./getScaleReports');
+const mock = require('./mock');
 const countedEvents = require('./countedEvents');
 
 export const Controller = {
